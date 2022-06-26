66427f4a7b753257aadcfdb3c4bf377e63764eac
Hyxogen <8938732+Hyxogen@users.noreply.github.com>
2022-06-26 07:56:16 +0200

    add log messages like the subject

--- ex00/Fixed.cpp
+++ ex00/Fixed.cpp
@@ -1,13 +1,22 @@
 #include "Fixed.h"
+#include <iostream>
 
 const int Fixed::POINT = 8;
 
-Fixed::Fixed() : _data(0) { }
-Fixed::Fixed(const Fixed &other) : _data(other._data) { }
+Fixed::Fixed() : _data(0) {
+	std::cout << "Default constructor called" << std::endl;
+}
 
-Fixed::~Fixed() { }
+Fixed::Fixed(const Fixed &other) : _data(other._data) {
+	std::cout << "Copy constructor called" << std::endl;
+}
+
+Fixed::~Fixed() {
+	std::cout << "Destructor called" << std::endl;
+}
 
 Fixed &Fixed::operator=(const Fixed &other) {
+	std::cout << "Copy operator called" << std::endl;
 	if (this == &other) {
 		return *this;
 	}
@@ -15,9 +24,11 @@ Fixed &Fixed::operator=(const Fixed &other) {
 }
 
 int Fixed::getRawBits() const {
+	std::cout << "getRawBits called" << std::endl;
 	return _data;
 }
 
 int Fixed::setRawBits(const int raw) {
+	std::cout << "setRawBits called" << std::endl;
 	_data = raw;
 }
