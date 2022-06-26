172e8763e92e8f4200b0de357a571267b1c37acb
Hyxogen <8938732+Hyxogen@users.noreply.github.com>
2022-06-26 07:50:13 +0200

    add Fixed header

---
+++ ex00/Fixed.hpp
@@ -0,0 +1,21 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+class Fixed {
+
+	int _data;
+	const static int POINT;
+
+public:
+	Fixed();
+	Fixed(const Fixed &other);
+
+	~Fixed();
+
+	Fixed &operator=(const Fixed &other);
+
+	int getRawBits() const;
+	int setRawBits(const int raw);
+};
+
+#endif
